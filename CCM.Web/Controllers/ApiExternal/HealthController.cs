ffc2ea9f7b8b4ea9ef4eb8e539d2eee6d50bcfe2
Alexander Gramner <alexander.gramner@sr.se>
2019-09-27 12:13:16 +0200

    Simple health api to use during deployment process

---
+++ CCM.Web/Controllers/ApiExternal/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace CCM.Web.Controllers.ApiExternal
+{
+    /// <summary>
+    /// For health checks (used for example during deploy process)
+    /// </summary>
+    public class HealthController: ApiController
+    {
+        public IHttpActionResult Get() => Ok();
+    }
+}
